31c63d64ddf68cca4b600ab79a429d76f946276f
Elmir-52 <elmircaraev21@gmail.com>
2026-06-22 01:48:13 +0300

    style: migrate header component to tailwind

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,4 @@
 import { HEADER_NAV_LINKS } from '../../shared/data/headerNavLinks';
-import './Header.scss';
 import { Link, useLocation } from 'react-router';
 
 export default function Header() {
@@ -7,14 +6,20 @@ export default function Header() {
     const currentPath = location.pathname;
 
     return (
-        <header className='header'>
-            <nav className="header__navigation">
+        <header className='flex justify-center items-center h-25 sticky top-0 z-10 mb-12.5 '>
+            <nav 
+                className="flex items-center gap-2.5 p-2.5 bg-white/10 backdrop-blur-md 
+                shadow-2xl rounded-4xl"
+            >
                 {
                     HEADER_NAV_LINKS.map(link => {
                         return <Link 
                             key={link.path} 
                             to={link.path} 
-                            className={currentPath === link.path ? 'header__nav-link header__nav-link_active' : 'header__nav-link'}
+                            className={currentPath === link.path ? 
+                                'text-xl px-5 py-2.5 rounded-3xl cursor-pointer bg-black text-white' : 
+                                'text-xl px-5 py-2.5 rounded-3xl cursor-pointer hover:bg-black hover:text-white'
+                            }
                         >{link.name}</Link>
                     })
                 }
