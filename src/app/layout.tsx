09d7ea57b5e5b658a1890c500ce7fa0a02c239af
Elmir-52 <elmircaraev21@gmail.com>
2026-08-04 23:53:29 +0400

    refactor: move Header to nextjs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
 import type { Metadata } from 'next'
 import './globals.css';
+import Header from '../components/Header/Header';
+import { Roboto } from 'next/font/google'
+import { NextFontWithVariable } from 'next/dist/compiled/@next/font';
+
+const roboto: NextFontWithVariable = Roboto({
+    subsets: ['cyrillic', 'latin'],
+    variable: '--font-roboto'
+});
 
 export const metaData: Metadata = {
     title: 'Dinopage',
@@ -12,8 +20,12 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="en">
+        <html 
+            lang="en"
+            className={`${roboto.className}`}
+        >
             <body>
+                <Header />
                 {children}
             </body>
         </html>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,33 @@
+'use client'
+
+import { usePathname } from 'next/navigation';
 import { HEADER_NAV_LINKS } from '../../shared/data/headerNavLinks';
-import { Link, useLocation } from 'react-router';
+import Link from 'next/link';
 
 export default function Header() {
-    const location = useLocation();
-    const currentPath = location.pathname;
+    const currentPath = usePathname();
 
     return (
-        <header className='font-[Nunito] font-semibold flex justify-center items-center h-25 sticky top-0 z-10 mb-12.5'>
+        <header 
+            className="font-semibold flex justify-center items-center h-25 
+            sticky top-0 z-10 mb-12.5"
+        >
             <nav 
                 className="flex items-center gap-2.5 p-2.5 bg-white/10 backdrop-blur-md 
                 shadow-2xl rounded-4xl"
             >
                 {
                     HEADER_NAV_LINKS.map(link => {
-                        return <Link 
+                        return <Link
                             key={link.path} 
-                            to={link.path} 
+                            href={link.path} 
                             className={currentPath === link.path ? 
                                 'text-xl px-5 py-2.5 rounded-3xl cursor-pointer bg-black text-white' : 
                                 'text-xl px-5 py-2.5 rounded-3xl cursor-pointer hover:bg-black hover:text-white'
                             }
-                        >{link.name}</Link>
+                        >
+                            {link.name}
+                        </Link>
                     })
                 }
             </nav>
