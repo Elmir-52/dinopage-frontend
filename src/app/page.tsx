c8240bfbe3d9e16772f23e26bd4cbfa7ac698509
Elmir-52 <elmircaraev21@gmail.com>
2026-08-07 15:43:01 +0300

    refactor: move HomePage imports to alias

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import HomeSection from "../components/HomeSection/HomeSection";
+import HomeSection from "@/components/HomeSection/HomeSection";
 
 export default function HomePage() {
     return (
