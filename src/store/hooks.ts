9319f68613c255b0042ccd2512d942185bee2cce
Elmir-52 <elmircaraev21@gmail.com>
2026-08-08 12:31:49 +0300

    fix(redux): remove withTypes from useDispatch in hooks.ts

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,6 +1,8 @@
 import { useDispatch, useSelector, useStore } from "react-redux";
 import { AppDispatch, AppStore, RootState } from "./store";
 
-export const useAppDispatch = () => useDispatch.withTypes<AppDispatch>();
+// у useAppDispatch типизация старого формата, потому что withTypes почему-то некорретно 
+// работает для него
+export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector = useSelector.withTypes<RootState>();
 export const useAppStore = useStore.withTypes<AppStore>();
