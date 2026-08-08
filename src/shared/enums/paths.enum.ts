23ad81233ac979299a9e39e11f38f769c000e858
Elmir-52 <elmircaraev21@gmail.com>
2026-08-08 21:53:41 +0300

    feat: add Paths enum

---
+++ src/shared/enums/paths.enum.ts
@@ -0,0 +1,8 @@
+export const enum Paths {
+    MAIN = '/',
+    LOGIN = '/auth/login',
+    REGISTER = '/auth/register',
+    PROFILE = '/profile',
+    DOCS = '/docs',
+    PAGES = '/docs/pages'
+}
