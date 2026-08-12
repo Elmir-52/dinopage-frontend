796f3f9771da0d932dc0e59291a336aadd34d678
Elmir-52 <elmircaraev21@gmail.com>
2026-08-12 21:22:40 +0300

    feat: add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Paths } from "@/shared/enums/paths.enum";
 import { Metadata } from "next";
+import Image from "next/image";
 import Link from "next/link";
 
 export const metadata: Metadata = {
@@ -10,8 +11,36 @@ export const metadata: Metadata = {
 
 export default function HomePage() {
     return (
-        <Link href={Paths.DOCS}>
-            Go to docs
-        </Link>
+        <>
+            <header
+                className="w-[95%] h-25 sticky top-0 z-10 mx-auto mt-2.5 mb-16"
+            >
+                <nav 
+                    className="flex justify-between items-center p-2.5 bg-white/10 
+                    backdrop-blur-md shadow-2xl rounded-2xl"
+                    >
+                    <Link 
+                        className="p-2 rounded-xl cursor-pointer bg-gray-200
+                        border border-solid border-gray-400"
+                        href={Paths.MAIN}
+                    >
+                        <Image 
+                            src="/dino.png"
+                            width={26}
+                            height={26}
+                            alt="Dinopage logo"
+                        />
+                    </Link>
+                    
+                    <Link 
+                        className="px-5 py-2 rounded-xl cursor-pointer bg-black 
+                        text-white h-full"
+                        href={Paths.DOCS}
+                    >
+                        Open Dinopage
+                    </Link>
+                </nav>
+            </header>
+        </>
     )
 }
