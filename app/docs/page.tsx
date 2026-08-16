acecbf1665448f291ebb2cfec5ab4a9af4578274
Elmir-52 <elmircaraev21@gmail.com>
2026-08-16 11:51:59 +0300

    refactor: move HomeSection to /widgets

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,5 +1,5 @@
 import Header from "@/components/Header/Header";
-import HomeSection from "@/components/HomeSection/HomeSection";
+import HomeSection from "@/widgets/HomeSection";
 
 export default function DocsPage() {
     return (
