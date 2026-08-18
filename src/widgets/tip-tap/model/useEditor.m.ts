b958df859118b926d4b7088141c2c2819f0e716a
Elmir-52 <elmircaraev21@gmail.com>
2026-08-18 21:14:59 +0300

    feat: add useEditorModel

---
+++ src/widgets/tip-tap/model/useEditor.m.ts
@@ -0,0 +1,46 @@
+import { TextStyleKit } from "@tiptap/extension-text-style";
+import { useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { useEffect, useRef } from "react";
+import { CustomHeading } from "../lib/tiptapExtensions/custom-heading";
+import Code from "@tiptap/extension-code";
+import { Note } from "@/shared/model";
+
+export function useEditorModel(note: Note) {
+    const noteTitleInputRef = useRef<HTMLInputElement | null>(null);
+
+    const editor = useEditor({
+        extensions: [
+            TextStyleKit,
+            StarterKit.configure({
+                // Отключаем встроенный heading, чтобы он не конфликтовал с CustomHeading
+                heading: false,
+            }),
+            CustomHeading,
+
+            Code.configure({
+                HTMLAttributes: {
+                    class: 'bg-gray-200 px-2 py-[1px] border-solid border-black rounded-md text-red-600',
+                },
+            }),
+        ],
+        editorProps: {
+            attributes: {
+                class: 'bg-white w-full min-h-full px-15 py-3 pb-50 focus:outline-0'
+            },
+        },
+        content: note.content ? JSON.parse(note.content) : '',
+        immediatelyRender: false,
+    });
+
+    useEffect(() => {
+        if (noteTitleInputRef.current) {
+            noteTitleInputRef.current.value = note.title;
+        }
+    }, []);
+
+    return {
+        editor,
+        noteTitleInputRef
+    }
+}
